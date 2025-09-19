68cc722dcea7a4de5fb7efe5e9c3672e748ce777
Jhas-Tin <jhastingithub@gmail.com>
2025-09-19 11:30:01 +0800

    fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,12 @@
-// src/middleware.ts in cloud-system-a
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-import type { NextRequest } from "next/server";
+import { clerkMiddleware } from '@clerk/nextjs/server';
 
-const isPublicRoute = createRouteMatcher(["/api/images", "/favicon.ico", "/public(.*)"]);
-
-export default clerkMiddleware(async (auth, req: NextRequest) => {
-  // If the request path is NOT in public routes, then protect
-  if (!isPublicRoute(req)) {
-    await auth.protect();
-  }
-});
+export default clerkMiddleware();
 
 export const config = {
   matcher: [
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    "/(api|trpc)(.*)",
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
   ],
 };
